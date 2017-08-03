e8cce3af797ac6941f1995998bca437958952dc7
ClansChen <clanschen@gmail.com>
2017-08-03 17:15:33 +0800

    改进生成转码表方法

--- Plugin/dllmain.cpp
+++ Plugin/dllmain.cpp
@@ -4,31 +4,34 @@
 
 void UTF8ToLatin1View()
 {
-	std::vector<char> u8sequence;
-	std::vector<char> latin1s(0x10000);
-	std::vector<std::pair<std::uint32_t, char>> result(0xFFFF);
+	std::vector<char> u8sequence; 
+	std::vector<std::uint32_t> u32sequence;
+	std::vector<unsigned char> latin1s(0x10000);
+	std::vector<std::pair<std::uint32_t, char>> result;
 
 	for (std::uint32_t cp = 1; cp < 0x10000; ++cp)
 	{
-		utf8::unchecked::append(cp, std::back_inserter(u8sequence));
+		u32sequence.push_back(cp);
 	}
+
+	utf8::unchecked::utf32to8(u32sequence.begin(), u32sequence.end(), std::back_inserter(u8sequence));
 
 	u8sequence.push_back(0);
 
-	injector::fastcall<void(const char *, char *)>::call(game.pfConvertUTF8ToLatin1, u8sequence.data(), latin1s.data());
+	injector::fastcall<void(const char *, unsigned char *)>::call(game.pfConvertUTF8ToLatin1, u8sequence.data(), latin1s.data());
 
 	latin1s.pop_back();
 
-	for (std::size_t index = 0; index < latin1s.size(); ++index)
+	for (std::uint32_t index = 0; index < latin1s.size(); ++index)
 	{
-		result[index].first = index + 1;
-		result[index].second = latin1s[index];
-	}
+		std::uint32_t cp = u32sequence[index];
+		unsigned char latin1 = latin1s[index];
 
-	result.erase(std::remove_if(result.begin(), result.end(),
-		[](std::pair<std::uint32_t, char> &p) {
-		return (p.first > 255) && (p.second == '?'); }),
-		result.end());
+		if (cp != latin1 && (cp < 256 || latin1 != '?'))
+		{
+			result.emplace_back(cp, latin1);
+		}
+	}
 
 	std::cout << result.size();
 }
@@ -37,9 +40,9 @@ BOOL WINAPI DllMain(HMODULE module, DWORD reason, void *reserved)
 {
 	if (reason == DLL_PROCESS_ATTACH)
 	{
-		Plugin::Init(module);
+		//Plugin::Init(module);
 
-		//UTF8ToLatin1View();
+		UTF8ToLatin1View();
 	}
 
 	return TRUE;
